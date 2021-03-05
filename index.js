42b1b7a8cf1e5cb6e3f5868bdea4c15b7dbb504c
Guilherme Brunholi <gui_brunholi@hotmail.com>
2021-03-04 23:26:53 -0300

    ajuste

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
-const express = require ('express')
-const handlebars =  require ('express-handlebars')
-const bodyparser = require ('body-parser')
+const express = require('express')
+const handlebars = require('express-handlebars')
+const bodyparser = require('body-parser')
 const app = express()
 const port = 3081
 
-app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
-app.use(bodyparser.urlencoded({extended: false}))
+app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
 
 var resumogeral = []
 
 class produto {
-    constructor(codigo,descricao,quantidade,preco)
-    {
+    constructor(codigo, descricao, quantidade, preco) {
         this.codigo = codigo
         this.descricao = descricao
         this.quantidade = quantidade
         this.preco = preco
     }
-
 }
 
-app.get('/',(req, res) =>{
-    res.send(resumogeral)
+app.get('/', (req, res) => {
+    res.render('all', { lista: resumogeral })
 })
 
-app.get('/cadproduto',(req, res) =>{
+app.get('/cadproduto', (req, res) => {
     res.render('cadproduto')
 })
 
-app.post('/retornocadproduto',(req, res) => {
+app.post('/retornocadproduto', (req, res) => {
     const produtounico1 = req.body.codigo
     const produtounico2 = req.body.descricao
     const produtounico3 = req.body.quantidade
@@ -42,22 +40,67 @@ app.post('/retornocadproduto',(req, res) => {
     const quantidade = produtounico3.toString()
     const preco = produtounico4.toString()
 
-    var x = new produto(produtounico1.toString(),produtounico2.toString(),produtounico3.toString(), produtounico4.toString())
-
-    
-
+    var x = new produto(produtounico1.toString(), produtounico2.toString(), produtounico3.toString(), produtounico4.toString())
 
     if (codigo.length == 0)
-    //conteudo que eu quero enviar
-        res.render('produtounico', {msg: 'Mensagem Inválida'})
-    else{
-        resumogeral.push(x)
+        //conteudo que eu quero enviar
+        res.render('produtounico', { msg: 'Mensagem Inválida' })
+    else {
+        resumogeral.push(x);
 
-        res.render('produtounico',{msg: 'Inclusão Concluída com sucesso!'})
+        res.render('produtounico', { msg: 'Inclusão Concluída com sucesso!' })
     }
-
 })
 
+app.get('/produto/:codigo', (req, res) => {
+    try {
+        resumogeral.forEach(item => item.codigo == req.params.codigo ? res.send(item) : res.render('produtounico', { msg: 'Não tem' }));
+    } catch (error) {
+        res.render(error);
+    }
+})
 
+app.post('/getProduto/:codigo', (req, res) => {
+    try {
+        resumogeral.forEach(item => {
+            if (item.codigo == req.params.codigo) {
+                res.render('editproduto', { codigo: item.codigo, descricao: item.descricao, quantidade: item.quantidade, preco: item.preco })
+            }
+        });
+    } catch (error) {
+        res.render(error);
+    }
+})
+
+app.post('/edit/:codigo', (req, res) => {
+    try {
+        resumogeral.forEach(item => {
+            if (item.codigo == req.params.codigo) {
+                
+                item.descricao = req.body.descricao
+                item.quantidade = req.body.quantidade
+                item.preco = req.body.preco
+            }
+        });
+        res.redirect('/')
+    } catch (error) {
+        res.render('produtounico', { msg: error })
+    }
+});
+
+app.post('/delete/:codigo', (req, res) => {
+    try {
+        var newList = []
+        resumogeral.forEach(item => {
+            if (item.codigo != req.params.codigo) {
+                newList.push(item)
+            }
+        });
+        resumogeral = newList;
+        res.redirect('/')
+    } catch (error) {
+        res.render('produtounico', { msg: error })
+    }
+})
 
 app.listen(port)
